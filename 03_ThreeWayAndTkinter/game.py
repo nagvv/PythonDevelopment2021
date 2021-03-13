7cbd1fb59df9ef6cf92bd9e2da05b25d167595f7
Alexey Nikolashkin <alexey.nikolashkin@tevian.ru>
2021-03-13 20:51:55 +0300

    implement new game button

--- 03_ThreeWayAndTkinter/game.py
+++ 03_ThreeWayAndTkinter/game.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from random import shuffle
 
 WINDOW_MIN_WIDTH = 200
 WINDOW_MIN_HEIGHT = 200
@@ -14,22 +15,31 @@ class App(tk.Frame):
         for i in range(4):
             self.columnconfigure(i, weight=1)
             self.rowconfigure(i + 1, weight=1)
+        self.emptyCell = (0, 0)
         self.createControlButtons()
         self.createNumButtons()
+        self.newGame()
 
     def createControlButtons(self):
-        self.newButton = tk.Button(self, text='New', height=CONTROL_BUTTON_HEIGHT, width=CONTROL_BUTTON_WIDTH)
+        self.newButton = tk.Button(self, text='New', command=self.newGame, height=CONTROL_BUTTON_HEIGHT, width=CONTROL_BUTTON_WIDTH)
         self.newButton.grid(row=0, column=0, columnspan=2, pady=CONTROL_BUTTON_PAD)
         self.exitButton = tk.Button(self, text='Exit', command=self.quit, height=CONTROL_BUTTON_HEIGHT, width=CONTROL_BUTTON_WIDTH)
         self.exitButton.grid(row=0, column=2, columnspan=2, pady=CONTROL_BUTTON_PAD)
 
     def createNumButtons(self):
         for i in range(1, 16):
-            btn_name = f'numButton{i}'
-            row = 1 + i // 4
-            col = i % 4
-            setattr(self, btn_name, tk.Button(self, text=str(i), height=NUM_BUTTON_HEIGHT, width=NUM_BUTTON_WIDTH))
-            getattr(self, btn_name).grid(row=row, column=col, sticky='nsew')
+            setattr(self, f'numButton{i}', tk.Button(self, text=str(i), height=NUM_BUTTON_HEIGHT, width=NUM_BUTTON_WIDTH))
+
+    def newGame(self):
+        positions = [i for i in range(16)] # 0 - empty cells, others are number cells with corresponding numbers
+        shuffle(positions) # TODO: check solvability and if not rotate numbers
+        for pos, i in enumerate(positions):
+            row = 1 + pos // 4
+            col = pos % 4
+            if i == 0:
+                self.emptyCell = (col, row)
+                continue
+            getattr(self, f'numButton{i}').grid(row=row, column=col, sticky='nsew')
 
 if __name__ == "__main__":
     window = tk.Tk()
